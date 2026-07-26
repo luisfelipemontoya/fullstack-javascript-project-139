1c9ef86998c5ccaddb8df03d2ef8b75d8d685c78
luisfelipemontoya <luisfelipemontoya88@gmail.com>
2026-07-25 22:37:42 -0500

    feat: add login form with Formik

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,5 +1,46 @@
+import { Formik, Form, Field } from 'formik';
+
 function LoginPage() {
-    return <h1>Login</h1>
+    return (
+        <>
+            <h1>Iniciar sesión</h1>
+
+            <Formik
+                initialValues={{
+                    username: '',
+                    password: '',
+                }}
+                onSubmit={(values) => {
+                    console.log(values);
+                }}
+            >
+                <Form>
+                    <div>
+                        <label htmlFor="password">Usuario</label>
+                        <Field
+                            id="username"
+                            name="username"
+                            type="text"
+                        />
+                    </div>
+
+                    <div>
+                        <label htmlFor="password">Contraseña</label>
+                        <Field
+                            id="password"
+                            name="password"
+                            Type="text"
+                        />
+                    </div>
+
+                    <button type="submit">
+                        Entrar
+                    </button>
+                </Form>
+            </Formik>
+        </>
+
+    );
 }
 
 export default LoginPage;
