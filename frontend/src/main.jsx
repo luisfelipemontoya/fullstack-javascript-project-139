3d6d7aa5016bd14ba5ddacae86b4a6331dc91b04
luisfelipemontoya <luisfelipemontoya88@gmail.com>
2026-08-09 17:52:12 -0500

    feat: integrate rollbar error tracking

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,17 +2,23 @@ import './i18n';
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux';
+import { Provider as RollbarProvider, ErrorBoundary } from '@rollbar/react';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css'
 
 import App from './App.jsx'
 import store from './store/index.js';
+import rollbarConfig from './rollbar.js';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <RollbarProvider config={rollbarConfig}>
+      <Provider store={store}>
+        <ErrorBoundary>
+          <App />
+        </ErrorBoundary>
+      </Provider>
+    </RollbarProvider>
   </StrictMode>,
 )
---
+++ frontend/src/rollbar.js
@@ -0,0 +1,6 @@
+const rollbarConfig = {
+	accessToken: import.meta.env.VITE_ROLLBAR_ACCESS_TOKEN,
+	environment: 'production',
+};
+
+export default rollbarConfig;
